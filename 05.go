3e91998b52371710cd6519ee239dc3fa0d2be6ab
aarzilli <alessandro.arzilli@gmail.com>
2023-12-05 07:00:07 +0100

    day 5 cleanup + better algorithm for part 2

--- 05.go
+++ 05.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	. "aoc/util"
-	"fmt"
 	"os"
 )
-
-func pf(fmtstr string, any ...interface{}) {
-	fmt.Printf(fmtstr, any...)
-}
-
-func pln(any ...interface{}) {
-	fmt.Println(any...)
-}
 
 var maps = make(map[string][]mapping)
 
@@ -20,65 +11,77 @@ type mapping []int
 
 func main() {
 	groups := Input(os.Args[1], "\n\n", true)
-	pf("len %d\n", len(groups))
 	seeds := Vatoi(Spac(Spac(groups[0], ":", -1)[1], " ", -1))
-	pln(seeds)
 	for i := 1; i < len(groups); i++ {
 		group := groups[i]
 		lines := Spac(group, "\n", -1)
 		name := Spac(lines[0], " ", -1)[0]
-		//pln(name)
+
 		for j := 1; j < len(lines); j++ {
-			line := lines[j]
-			maps[name] = append(maps[name], Vatoi(Spac(line, " ", -1)))
+			maps[name] = append(maps[name], Vatoi(Spac(lines[j], " ", -1)))
 		}
 	}
-	//pln(maps)
 
 	minloc := -1
-
 	for _, seed := range seeds {
-		p := seedtoloc(seed)
+		p := seedtoloc(seed, nil)
 		if minloc == -1 || p < minloc {
 			minloc = p
 		}
 	}
 
-	Sol(minloc)
+	Sol(minloc, 331445006)
 
 	minloc = -1
 	for i := 0; i < len(seeds); i += 2 {
 		seedstart := seeds[i]
-		n := seeds[i+1]
+		seedend := seedstart + seeds[i+1]
 
-		for j := 0; j < n; j++ {
-			seed := seedstart + j
-			p := seedtoloc(seed)
+		seed := seedstart
+		for seed < seedend {
+			rng := seedend - seed
+			p := seedtoloc(seed, &rng)
 			if minloc == -1 || p < minloc {
 				minloc = p
 			}
+			seed += rng
 		}
 	}
 
-	Sol(minloc)
+	Sol(minloc, 6472060)
 }
 
-func applymap(amap []mapping, seeds int) int {
+func applymap(amap []mapping, in int, prng *int) int {
 	for i := range amap {
-		if amap[i][1] <= seeds && seeds < amap[i][1]+amap[i][2] {
-			return amap[i][0] + seeds - amap[i][1]
+		if amap[i][1] <= in && in < amap[i][1]+amap[i][2] {
+			if prng != nil {
+				rng := amap[i][1] + amap[i][2] - in
+				if rng < *prng {
+					*prng = rng
+				}
+			}
+			return amap[i][0] + in - amap[i][1]
 		}
 	}
-	return seeds
+	if prng != nil {
+		// find next mapping to update prng
+		for i := range amap {
+			if in < amap[i][1] {
+				rng := amap[i][1] - in
+				if rng < *prng {
+					*prng = rng
+				}
+			}
+		}
+	}
+	return in
 }
 
-func seedtoloc(seed int) int {
+func seedtoloc(seed int, prng *int) int {
 	p := seed
 	for _, mapname := range []string{"seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location"} {
-		np := applymap(maps[mapname], p)
-		//pln(p, mapname, np)
-		p = np
+		newp := applymap(maps[mapname], p, prng)
+		p = newp
 	}
-	//pln("location", p)
 	return p
 }
