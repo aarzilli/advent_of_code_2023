1a063d1b75082020d43f12afcca4979ebf58e11d
aarzilli <alessandro.arzilli@gmail.com>
2023-12-06 06:12:03 +0100

    day 6 + day 2 wolfram notebook

---
+++ 06.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	. "aoc/util"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func pf(fmtstr string, any ...interface{}) {
+	fmt.Printf(fmtstr, any...)
+}
+
+func pln(any ...interface{}) {
+	fmt.Println(any...)
+}
+
+func main() {
+	lines := Input(os.Args[1], "\n", true)
+	pf("len %d\n", len(lines))
+	times := Getints(lines[0], false)
+	dists := Getints(lines[1], false)
+	pln(times, dists)
+	
+	part1 := 1
+	for i := range times {
+		part1 *= enum(times[i], dists[i])
+	}
+	Sol(part1)
+	
+	// part2
+	p2time := Getints(strings.ReplaceAll(lines[0], " ", ""), false)[0]
+	p2dist := Getints(strings.ReplaceAll(lines[1], " ", ""), false)[0]
+	
+	Sol(enum(p2time, p2dist))
+}
+
+func enum(time, dist int) int {
+	r := 0
+	for tbtn := 1; tbtn < time; tbtn++ {
+		tm := dist / tbtn
+		if tbtn + tm < time {
+			//pf("button: %d remaining: %d\n", tbtn, tm)
+			r++
+		}
+	}
+	return r
+}
