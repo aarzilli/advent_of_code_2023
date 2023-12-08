6970b1fb26a7deaebeef66b47771f1d4b4366411
aarzilli <alessandro.arzilli@gmail.com>
2023-12-08 07:04:01 +0100

    day 8 full solution

--- 08.1.go
+++ 08.1.go
@@ -15,7 +15,7 @@ func pln(any ...interface{}) {
 }
 
 type node struct {
-	name string
+	name  string
 	child []string
 }
 
@@ -31,9 +31,9 @@ func main() {
 		v := Spac(line, "=", -1)
 		src := v[0]
 		v = Spac(v[1][1:len(v[1])-1], ",", -1)
-		M[src] = &node{ src, v }
+		M[src] = &node{src, v}
 	}
-	
+
 	cur := M["AAA"]
 	step := 0
 	i := 0
@@ -50,6 +50,63 @@ func main() {
 		step++
 		i++
 	}
-	
+
 	Sol(step)
+
+	curset := make(Set[string])
+	for i := range M {
+		if M[i].name[len(M[i].name)-1] == 'A' {
+			curset[M[i].name] = true
+		}
+	}
+
+	first := map[string]int{}
+
+	for name := range curset {
+		cur := M[name]
+		step := 0
+		i := 0
+		for {
+			m := instr[i%len(instr)]
+			if cur.name[len(cur.name)-1] == 'Z' {
+				if first[name] == 0 {
+					first[name] = step
+					break
+				}
+			}
+			if m == 'L' {
+				cur = M[cur.child[0]]
+			} else {
+				cur = M[cur.child[1]]
+			}
+			step++
+			i++
+		}
+	}
+
+	v := []int{}
+	for _, x := range first {
+		v = append(v, x)
+	}
+
+	Sol(LCM(v[0], v[1], v[2:]...))
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
--- 08.go
+++ 08.go
@@ -4,8 +4,6 @@ import (
 	. "aoc/util"
 	"fmt"
 	"os"
-	"sort"
-	"strings"
 )
 
 func pf(fmtstr string, any ...interface{}) {
@@ -17,7 +15,7 @@ func pln(any ...interface{}) {
 }
 
 type node struct {
-	name string
+	name  string
 	child []string
 }
 
@@ -33,16 +31,13 @@ func main() {
 		v := Spac(line, "=", -1)
 		src := v[0]
 		v = Spac(v[1][1:len(v[1])-1], ",", -1)
-		M[src] = &node{ src, v }
+		M[src] = &node{src, v}
 	}
-	
-	
-	/*
+
 	cur := M["AAA"]
 	step := 0
 	i := 0
 	for {
-		pln(cur.name, step)
 		m := instr[i%len(instr)]
 		if cur.name == "ZZZ" {
 			break
@@ -55,82 +50,18 @@ func main() {
 		step++
 		i++
 	}
-	
-	Sol(step)*/
-	
+
+	Sol(step)
+
 	curset := make(Set[string])
 	for i := range M {
 		if M[i].name[len(M[i].name)-1] == 'A' {
 			curset[M[i].name] = true
 		}
 	}
-	
-	/*seen := map[string]bool{}
-	
-	step := 0
-	i := 0
-	for {
-		signature := printset(curset)
-		pln(signature, len(seen))
-		if seen[signature] {
-			pln("repeated")
-			os.Exit(1)
-		}
-		seen[signature] = true
-		m := instr[i%len(instr)]
-		ok := true
-		for name := range curset {
-			if name[len(name)-1] != 'Z' {
-				ok = false
-				break
-			}
-		}
-		if ok {
-			break
-		}
-		
-		newset := make(Set[string])
-		for name := range curset {
-			var next string
-			if m == 'L' {
-				next = M[name].child[0]
-			} else {
-				next = M[name].child[1]
-			}
-			newset[next] = true
-		}
-		curset = newset
-		
-		step++
-		i++
-	}
-	
-	Sol(step)*/
-	
-	/*
-	for name := range curset {
-		cur := M[name]
-		step := 0
-		i := 0
-		for {
-			pln(step, cur.name)
-			m := instr[i%len(instr)]
-			if name[len(name)-1] == 'Z' {
-				pln("at", name, "after", step)
-			}
-			if m == 'L' {
-				cur = M[cur.child[0]]
-			} else {
-				cur = M[cur.child[1]]
-			}
-			step++
-			i++
-		}
-	}*/
-	
+
 	first := map[string]int{}
-	period := map[string]int{}
-	
+
 	for name := range curset {
 		cur := M[name]
 		step := 0
@@ -140,8 +71,6 @@ func main() {
 			if cur.name[len(cur.name)-1] == 'Z' {
 				if first[name] == 0 {
 					first[name] = step
-				} else if period[name] == 0 {
-					period[name] = step - first[name]
 					break
 				}
 			}
@@ -154,17 +83,30 @@ func main() {
 			i++
 		}
 	}
-	
-	pln(first)
-	pln(period)
-	
+
+	v := []int{}
+	for _, x := range first {
+		v = append(v, x)
+	}
+
+	Sol(LCM(v[0], v[1], v[2:]...))
 }
 
-func printset(curset Set[string]) string {
-	x := []string{}
-	for name := range curset {
-		x = append(x, name)
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
 	}
-	sort.Strings(x)
-	return strings.Join(x, " ")
+	return a
+}
+
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
