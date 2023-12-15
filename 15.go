d397b0bac3e95966194256d06ed765124095ec36
aarzilli <alessandro.arzilli@gmail.com>
2023-12-15 08:43:02 +0100

    day 15 better solution

--- 15.go
+++ 15.go
@@ -3,12 +3,12 @@ package main
 import (
 	. "aoc/util"
 	"os"
-	"strings"
+	"slices"
 )
 
 type lens struct {
 	kind string
-	pwr int
+	pwr  int
 }
 
 func main() {
@@ -18,63 +18,35 @@ func main() {
 		part1 += hash(x)
 	}
 	Sol(part1)
-	
-	box := make([][]lens, 256)
-	
-	for _, x := range Spac(lines[0], ",", -1) {
-		var tgt string
-		var op string
-		var arg int
-		
-		eq := strings.Index(x, "=")
-		if eq >= 0 {
-			v := Spac(x, "=", 2)
-			op = "="
-			tgt = v[0]
-			arg = Atoi(v[1])
+
+	fl := map[string]int{}
+	order := map[string]int{}
+
+	for i, x := range Spac(lines[0], ",", -1) {
+		v := Spac(x, "=", 2)
+		if len(v) == 2 {
+			if _, ok := fl[v[0]]; !ok {
+				order[v[0]] = i
+			}
+			fl[v[0]] = Atoi(v[1])
 		} else {
-			if x[len(x)-1] != '-' {
-				panic("blah")
-			}
-			tgt = x[:len(x)-1]
-			op = "-"
-		}
-		
-		tgtbox := hash(tgt)
-		
-		switch op {
-		case "=":
-			found := false
-			for i := range box[tgtbox] {
-				if box[tgtbox][i].kind == tgt {
-					box[tgtbox][i].pwr = arg
-					found = true
-				}
-			}
-			if !found {
-				box[tgtbox] = append(box[tgtbox], lens{ tgt, arg })
-			}
-		case "-":
-			idx := -1
-			for i := range box[tgtbox] {
-				if box[tgtbox][i].kind == tgt {
-					idx = i
-					break
-				}
-			}
-			if idx >= 0 {
-				copy(box[tgtbox][idx:], box[tgtbox][idx+1:])
-				box[tgtbox] = box[tgtbox][:len(box[tgtbox])-1]
-			}
+			delete(fl, x[:len(x)-1])
+			delete(order, x[:len(x)-1])
 		}
 	}
-	
+
+	orderv := []string{}
+	for k := range order {
+		orderv = append(orderv, k)
+	}
+	slices.SortFunc(orderv, func(a, b string) int { return order[a] - order[b] })
 	part2 := 0
-	for i := range box {
-		for j := range box[i] {
-			p := (i+1) * (j+1) * box[i][j].pwr
-			part2 += p
-		}
+	boxes := [256]int{}
+	for _, k := range orderv {
+		h := hash(k)
+		pos := boxes[h]
+		boxes[h]++
+		part2 += (h + 1) * (pos + 1) * fl[k]
 	}
 	Sol(part2)
 }
